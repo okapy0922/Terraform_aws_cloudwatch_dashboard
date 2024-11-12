96ec628c32b02fe7b3ae2f1c4160283f0f0bfb59
okada-t-www <okada.t+dev@world-wing.com>
2024-11-12 18:19:25 +0900

    first_commit

--- envs/dev-dashboard-7436/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "dev-glico-sunao-cw-dashboad-terraform-state" # S3バケット名を指定
-    region         = "ap-northeast-1"  # リージョンを指定
-    profile        ="LogMon-Admin" # 使用するプロファイル（AWSアカウント名）を指定
-    encrypt        = true
-    key            = "terraform/state/terraform.tfstate"  # stateファイルを保管するS3内ファイルパスを指定
-  }
-}
