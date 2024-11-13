8a788f69ff4fb921527f7ef04a02edd9f3565805
OkadaTakumi <159122642+okada-t-www@users.noreply.github.com>
2024-11-13 14:49:52 +0900

    Delete envs/dev directory

--- envs/dev/backend.tf
+++
@@ -1,9 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "cloudwatch-dashboad-terraform-dev" # S3バケット名を指定
-    region         = "ap-northeast-1"  # リージョンを指定
-    profile        ="okada-trail-setup" # 使用するプロファイル（AWSアカウント名）を指定
-    encrypt        = true
-    key            = "terraform/state/terraform.tfstate"  # stateファイルを保管するS3内ファイルパスを指定
-  }
-}
